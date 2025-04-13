ba974579cfb9124af1b059cd500e67aea3cd4421
CodeForFun7325 <johnnguyen7325@outlook.com>
2025-04-13 14:39:16 -0700

    remove card and gallery files

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import React from 'react';
 import './App.css';
 
 import SearchBar from './component/search/searchbar';
-import Gallery from './component/cards/gallery/gallery';
 
 function App() {
   return (
     <div className="App">
       <h1>PokéDex</h1>
       <SearchBar />
-      <Gallery />
     </div>
   );
 }
