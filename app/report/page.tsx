d8a4770e51ee399cafb3c4490a145eda1b8d3844
CodeForFun7325 <johnnguyen7325@outlook.com>
2025-05-18 13:53:13 -0700

    Add image preview

--- app/report/page.tsx
+++ app/report/page.tsx
@@ -1,14 +1,45 @@
+"use client"; 
+
+import React, { useState } from "react";
+
 import "./page.css"; 
 
 export default function ReportCiting() { 
+  const [image, setImage] = useState<string | null>(null); 
+
+  const setImageOnChange = (e : React.ChangeEvent<HTMLInputElement>) => { 
+    const file = e.target.files?.[0]; 
+
+    if (file) { 
+      const reader = new FileReader(); 
+      reader.onloadend = () => setImage(reader.result as string); 
+      reader.readAsDataURL(file); 
+    }
+  }
+
   return (
-    <div className="report-form">
+    <>
       <h1>Report Pokemon Citing</h1>
-      <br />
-      <form> 
-        <label htmlFor="name">Pokemon: </label>
-        <input id="name" type="text" name="name"/>
-      </form> 
-    </div>
+      <div className="report-form">
+
+        {/* Image Upload Section */}
+        <form className="image-upload">
+          <h2>Image</h2>
+          <br />
+          {image && <img src={image} alt="Uploaded image preview" id="imagePreview" />}
+          <input type="file" id="imageUpload" accept="image/*" onChange={setImageOnChange}/>
+        </form>
+
+        <br />
+
+        {/* Pokemon Details */}
+        <form className="pokemon-details"> 
+          <h2>Pokemon Details</h2>
+          <br />
+          <label htmlFor="name">Pokemon: </label>
+          <input id="name" type="text" name="name"/>
+        </form> 
+      </div>
+    </>
   ); 
 }
